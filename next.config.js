32645b47561cdee41efe809a48e9f459fb176170
naga7 <naga@wishup.co>
2023-11-23 15:25:35 +0530

    init

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    // async rewrites ()  {
+    //     return [
+    //       {
+    //         source: "/blog",
+    //         destination: "https://blog.nagasai.in/blog/",
+    //       },
+    //       {
+    //         source: "/blog/",
+    //         destination: "https://blog.nagasai.in/blog/",
+    //       },
+    //     ];
+    //   }
+}
 
 module.exports = nextConfig
