69eb9e38f8f954e123653423b9407c54fcfcfd63
naga7 <naga@wishup.co>
2023-11-23 15:43:19 +0530

    test

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <h1 className="border-collapse text-4xl"> Naga Sai  <span className='text-xs cursor-pointer ' ><Link href={'/blog'}>Blog</Link></span> </h1>
-        <span><sup className='text-xs'>Software Engineer</sup></span>
+        <span><sup className='text-xs'>Software Engineer | IIT Madras</sup></span>
     </main>
   )
 }
--- next.config.js
+++ next.config.js
@@ -12,6 +12,7 @@ const nextConfig = {
     //       },
     //     ];
     //   }
+    // ok
 }
 
 module.exports = nextConfig
