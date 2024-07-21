fd13d982209761a03643628088970e3bcbb0a860
Naga Sai <naga@wishup.co>
2024-07-21 18:20:17 +0530

    #fixes

---
+++ app/_components/Generic/Card.tsx
@@ -0,0 +1,107 @@
+import React from "react";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
+import Link from "next/link";
+import { FiClock } from "react-icons/fi";
+
+type CardProps = {
+  type: "blog" | "project";
+  title: string;
+  description: string;
+  content?: string;
+  technologies?: string[];
+  imageUrl: string;
+  slug?: string;
+  publishedDate?: string;
+  tags?: string[];
+};
+
+export default function Card({
+  type,
+  title,
+  description,
+  content,
+  technologies,
+  imageUrl,
+  slug,
+  publishedDate,
+  tags,
+}: CardProps) {
+  const averageReadingSpeed = 200; // Words per minute
+  const wordCount = description.split(" ").length;
+  const readingTime = Math.ceil(wordCount / averageReadingSpeed);
+
+  return (
+    <div
+      className="bg-white shadow-md hover:shadow-lg rounded-lg overflow-hidden flex flex-col transition duration-300 ease-in-out transform hover:-translate-y-1"
+      style={{ minHeight: "400px" }}
+    >
+      <div className="flex-grow flex justify-center items-center h-40 relative">
+        <div className="absolute z-10 top-0 right-0 bg-black bg-opacity-75 text-white px-3 py-2 text-xs font-secondaryFont font-medium rounded-lg shadow">
+          <div className="flex items-center">
+            <FiClock className="inline-block w-4 h-4 mr-2" />
+            {readingTime} min read
+          </div>
+        </div>
+        <Image
+          src={imageUrl}
+          alt={title}
+          fill
+          objectFit="cover"
+          className="z-0"
+        />
+      </div>
+      <div className="p-4 md:p-2 flex-grow">
+        <h3 className="font-primartFont font-primaryFontWeight text-h3 mb-1 truncate">
+          {title}
+        </h3>
+        {type === "blog" && (
+          <div className="text-gray-600 text-xs md:text-sm mb-2">
+            {publishedDate}
+          </div>
+        )}
+        <p
+          className="text-gray-600 text-sm md:text-base mb-2 overflow-ellipsis overflow-hidden font-secondaryFont font-secondaryFontWeight"
+          style={{
+            display: "-webkit-box",
+            WebkitLineClamp: 3,
+            WebkitBoxOrient: "vertical",
+          }}
+        >
+          {description}
+        </p>
+        <div className="mb-2 flex flex-wrap">
+          {(type === "project" ? technologies : tags)
+            ?.slice(0, 5)
+            .map((item, index) => (
+              <span
+                key={index}
+                className={`inline-block rounded-full px-2 py-1 text-xs md:text-sm font-secondaryFont text-gray-700 mr-1 mb-1 ${
+                  type === "blog"
+                    ? "bg-blue-200 hover:bg-blue-300 cursor-pointer"
+                    : "bg-gray-200 hover:bg-gray-300"
+                }`}
+              >
+                {type === "blog" ? (
+                  <Link href={`/blog/tags/${item}`}>{item}</Link>
+                ) : (
+                  item
+                )}
+              </span>
+            ))}
+          {((type === "project" ? technologies : tags) || [])?.length > 5 && (
+            <span className="inline-block bg-gray-200 rounded-full px-2 py-1 text-xs md:text-sm font-secondaryFont text-gray-700 mr-1 mb-1">
+              +{((type === "project" ? technologies : tags) || []).length - 5}{" "}
+              more
+            </span>
+          )}
+        </div>
+        <Button className="font-medium w-full md:w-auto bg-green-500 text-white py-2.5 px-5 text-base rounded-md hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-700 focus:ring-opacity-50 shadow hover:shadow-lg transition ease-in-out duration-150">
+          {type === "project" ? "View Project" : "Read More"}{" "}
+          <MdOutlineKeyboardArrowRight className="ml-2 w-5 h-5" />
+        </Button>
+      </div>
+    </div>
+  );
+}
--- app/_components/Projects/Projects.tsx
+++ app/_components/Projects/Projects.tsx
@@ -1,11 +1,8 @@
 import React from "react";
-import Image from "next/image";
-import { Button } from "@/components/ui/button";
-import { FiArrowRight } from "react-icons/fi";
-import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 
+import { FiArrowRight } from "react-icons/fi";
+import Card from "../Generic/Card";
 import Link from "next/link";
-import ProjectCss from "@/public/css-modules/Project.module.css"; // Import the CSS module
 import {
   Carousel,
   CarouselContent,
@@ -27,14 +24,14 @@ const projects = [
       "Linux",
     ],
     imageUrl: "https://picsum.photos/400",
-    projectUrl: "#",
+    url: "#",
   },
   {
     title: "Project Two",
     description: "A mobile app that revolutionizes the way we share moments.",
     technologies: ["Flutter", "Firebase", "Dart"],
     imageUrl: "https://picsum.photos/200",
-    projectUrl: "#",
+    url: "#",
   },
   {
     title: "Project Three",
@@ -42,7 +39,7 @@ const projects = [
       "A web platform that connects freelancers with projects using AI matchmaking.",
     technologies: ["Vue.js", "Python", "Flask", "PostgreSQL"],
     imageUrl: "https://picsum.photos/200",
-    projectUrl: "#",
+    url: "#",
   },
   {
     title: "Project Four",
@@ -50,11 +47,24 @@ const projects = [
       "An e-commerce platform with a focus on sustainability and green products.",
     technologies: ["Next.js", "Stripe", "MongoDB"],
     imageUrl: "https://picsum.photos/200",
-    projectUrl: "#",
+    url: "#",
   },
   // Add more projects as needed
 ];
+import ProjectLayout from "./ProjectLayout";
 export default function Projects() {
+  const projects__ = projects.map((project, index) => (
+    <ProjectLayout key={index}>
+      <Card
+        type="project"
+        title={project.title}
+        description={project.description}
+        technologies={project.technologies}
+        imageUrl={project.imageUrl}
+        slug={project.url}
+      />
+    </ProjectLayout>
+  ));
   return (
     <div className="bg-grey-50 py-12 px-4 sm:px-6 lg:px-8 overflow-x-hidden">
       <div className="max-w-screen-xl mx-auto ">
@@ -70,67 +80,7 @@ export default function Projects() {
               },
             }}
           >
-            <CarouselContent className="py-2 ">
-              {projects.map((project, index) => (
-                <CarouselItem
-                  key={index}
-                  className="flex flex-col md:basis-1/2 lg:basis-1/3 sm:basis-1"
-                >
-                  <div
-                    className={`bg-white shadow-md hover:shadow-lg rounded-lg overflow-hidden flex flex-col  transition duration-300 ease-in-out transform hover:-translate-y-1`}
-                    style={{ minHeight: "400px" }}
-                  >
-                    <div className="flex-grow flex justify-center items-center h-40 relative">
-                      <div className="absolute inset-0 bg-gradient-to-t from-transparent to-gray-100 opacity-75"></div>
-                      <Image
-                        src={project.imageUrl}
-                        alt={project.title}
-                        fill
-                        objectFit="cover"
-                        className="z-10"
-                      />
-                    </div>
-                    <div className="p-4 md:p-2 flex-grow">
-                      <h3 className="font-primartFont font-primaryFontWeight  text-h3 mb-1 truncate">
-                        {project.title}
-                      </h3>
-                      <p
-                        className="text-gray-600 text-sm md:text-base mb-2 overflow-ellipsis overflow-hidden font-secondaryFont font-secondaryFontWeight"
-                        style={{
-                          maxHeight: "4.5em",
-                          display: "-webkit-box",
-                          WebkitLineClamp: 3,
-                          WebkitBoxOrient: "vertical",
-                        }}
-                      >
-                        {project.description}
-                      </p>
-                      <div className="mb-2 flex flex-wrap">
-                        {project.technologies
-                          .slice(0, 5)
-                          .map((tech, techIndex) => (
-                            <span
-                              key={techIndex}
-                              className="inline-block bg-gray-200 rounded-full px-2 py-1 text-xs md:text-sm font-secondaryFont text-gray-700 mr-1 mb-1 hover:bg-gray-300 cursor-pointer"
-                            >
-                              {tech}
-                            </span>
-                          ))}
-                        {project.technologies.length > 5 && (
-                          <span className="inline-block bg-gray-200 rounded-full px-2 py-1 text-xs md:text-sm font-secondaryFont text-gray-700 mr-1 mb-1">
-                            +{project.technologies.length - 5} more
-                          </span>
-                        )}
-                      </div>
-                      <Button className="font-secondaryFont w-full md:w-auto bg-green-500 text-white py-2 px-4 text-sm md:text-base rounded hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-700 focus:ring-opacity-50 shadow-md hover:shadow-lg transform hover:-translate-y-1 transition ease-in-out duration-150">
-                        View Project{" "}
-                        <MdOutlineKeyboardArrowRight className="ml-2 w-4 h-4 md:w-5 md:h-5" />
-                      </Button>
-                    </div>
-                  </div>
-                </CarouselItem>
-              ))}
-            </CarouselContent>
+            <CarouselContent className="py-2 ">{projects__}</CarouselContent>
             <CarouselPrevious />
             <CarouselNext />
           </Carousel>
