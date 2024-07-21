2563ab2b02c09824855fb0f6210cff6f0df3edbd
Naga Sai <naga@wishup.co>
2024-07-21 22:26:38 +0530

    #fixes

--- app/_components/NavBar.tsx
+++ app/_components/NavBar.tsx
@@ -8,7 +8,7 @@ export default function NavBar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
-    <nav className="bg-white  px-5 py-3 flex flex-col md:flex-row items-center justify-between shadow-lg transform -skew-y-1 md:sticky md:top-0 md:z-50">
+    <nav className="bg-white  px-5 py-3 flex flex-col md:flex-row items-center justify-between shadow-lg transform -skew-y-1 lg:sticky lg:top-0 lg:z-50">
       <div className="w-full flex justify-between items-center">
         <div className="flex items-center gap-2">
           <Image src={"/images/nagasai_in_logo.svg"} className="hidden md:block" alt="Logo" width={50} height={50} />
@@ -18,7 +18,7 @@ export default function NavBar() {
         <button className="block md:hidden" onClick={() => setIsMenuOpen(!isMenuOpen)}>
           <HiMiniBars3BottomLeft className="h-6 w-6" />
         </button>
-       <div className="hidden md:block">
+       <div className="hidden lg:block">
        <Eyes />
        </div>
       </div>
