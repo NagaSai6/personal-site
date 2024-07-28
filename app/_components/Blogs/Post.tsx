bc7a47a3a28216bb56f15c6cae67341ecb7f780e
Naga Sai <naga@wishup.co>
2024-07-28 19:51:33 +0530

    #fix

--- app/_components/Blogs/Post.tsx
+++ app/_components/Blogs/Post.tsx
@@ -37,25 +37,45 @@ const components: PortableTextComponents = {
   },
   block: {
     // Ex. 1: customizing common block types
-    h1: ({ children }) => <h1 className="text-h1 font-secondaryFontWeight">{children}</h1>,
-    h2: ({ children }) => <h2 className="text-h2 font-secondaryFontWeight">{children}</h2>,
-    h3: ({ children }) => <h3 className="text-h3">{children}</h3>,
-    h4: ({ children }) => <h4 className="text-h4">{children}</h4>,
-    h5: ({ children }) => <h5 className="text-h5">{children}</h5>,
-    h6: ({ children }) => <h6 className="text-h6">{children}</h6>,
-    span : ({ children }) => <span className="text-base">{children}</span>,
-    p : ({ children }) => <p className="text-base">{children}</p>,
+    h1: ({ children }) => (
+      <h1 className="text-h1 font-secondaryFontWeight my-4">{children}</h1>
+    ),
+    h2: ({ children }) => (
+      <h2 className="text-h2 font-secondaryFontWeight my-4">{children}</h2>
+    ),
+    h3: ({ children }) => (
+      <h3 className="text-h3 font-secondaryFontWeight my-4">{children}</h3>
+    ),
+    h4: ({ children }) => (
+      <h4 className="text-h3 font-primaryFontWeight  mt-4 mb-3">{children}</h4>
+    ),
+    h5: ({ children }) => (
+      <h5 className="text-h5 font-secondaryFontWeight my-4">{children}</h5>
+    ),
+    h6: ({ children }) => (
+      <h6 className="text-h6 font-secondaryFontWeight my-4">{children}</h6>
+    ),
+    span: ({ children }) => (
+      <span className="text-base font-secondaryFontWeight">{children}</span>
+    ),
+    normal: ({ children }) => (
+      <p className="text-base my-2  leading-normal">{children}</p>
+    ),
   },
   list: {
     // Ex. 1: customizing common list types
-    bullet: ({children}) => <ul className="mt-xl">{children}</ul>,
-    number: ({children}) => <ol className="mt-lg">{children}</ol>,
+    bullet: ({ children }) => (
+      <ul className="list-disc list-inside my-3 ml-4">{children}</ul>
+    ),
+    number: ({ children }) => (
+      <ol className="list-decimal list-inside my-2 ml-4">{children}</ol>
+    ),
   },
 };
 
 export default function Post({ post }: { post: BLOG_QUERYResult }) {
   return (
-    <div className="p-2 md:p-8 bg-white  shadow-lg rounded-lg">
+    <div className="p-0 md:p-8 bg-white  shadow-lg rounded-lg">
       <PostHeader
         title={post?.title!}
         author={{
